1a16129c98b10ffc9cc681908faf21caee04117a
Arshad <arshadmohd0852@gmail.com>
2025-07-15 22:32:43 +0530

    few changes

---
+++ Bit_Manipulation/string2binary.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string converttoBinary(int n) {
+    string res = "";
+    if (n == 0) return "0";  // edge case 
+    while (n > 0) {
+        if (n % 2 == 1) {
+            res += '1';  // '1' ko joda jaega
+        } else {
+            res += '0';  // nhi to '0' ko
+        }
+        n = n / 2;
+    }
+    reverse(res.begin(), res.end());  // string ko reverse kia jaega after loop kyuki answer ulta mil rha h
+    return res;
+}
+
+int main() {
+    int n = 45;  
+    cout << converttoBinary(n) << endl;  
+    return 0;
+}
+

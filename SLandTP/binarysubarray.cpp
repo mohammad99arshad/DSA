b92c7cd90a85c0f3c3e992f689fb3a5c91afd66c
Arshad <arshadmohd0852@gmail.com>
2025-08-03 18:33:37 +0530

    minor changes

--- SLandTP/binarysubarray.cpp
+++ SLandTP/binarysubarray.cpp
@@ -1,24 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-    int countAtMost(vector<int>& nums, int goal) {
-        int l = 0, r = 0, count = 0, sum = 0;
-        for (r = 0; r < nums.size(); ++r) {
-            sum += nums[r];
-            while (sum > goal && l <= r) {
-                sum -= nums[l++];
-            }
-            count += (r - l + 1);
+int countAtMost(vector<int> &nums, int goal)
+{
+    int l = 0, r = 0, count = 0, sum = 0;
+    for (r = 0; r < nums.size(); ++r)
+    {
+        sum += nums[r];
+        while (sum > goal && l <= r)
+        {
+            sum -= nums[l++];
         }
-        return count;
+        count += (r - l + 1);
     }
+    return count;
+}
 
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
-        if (goal < 0) return 0;  // Important edge case
-        return countAtMost(nums, goal) - countAtMost(nums, goal - 1);
-    }
-int main() {
+int numSubarraysWithSum(vector<int> &nums, int goal)
+{
+    if (goal < 0)
+        return 0; // Important edge case
+    return countAtMost(nums, goal) - countAtMost(nums, goal - 1);
+}
+int main()
+{
     vector<int> nums = {1, 0, 1, 0, 1};
     int goal = 2;
     int result = numSubarraysWithSum(nums, goal);
